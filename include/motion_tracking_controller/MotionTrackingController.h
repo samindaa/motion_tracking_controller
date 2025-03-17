ea6fc159eccbdf602c1eb40c53813a35083b31dc
qiayuanl <qiayuanl@berkeley.edu>
2025-03-16 20:08:21 -0700

    Add DataLogger

--- include/motion_tracking_controller/MotionTrackingController.h
+++ include/motion_tracking_controller/MotionTrackingController.h
@@ -1,18 +1,25 @@
 #pragma once
 
-#include "legged_rl_controllers/OnnxController.h"
+#include <legged_rl_controllers/OnnxController.h>
+
+#include "motion_tracking_controller/DataLogger.h"
 #include "motion_tracking_controller/common.h"
 
 namespace legged {
 class MotionTrackingController : public OnnxController {
  public:
+  controller_interface::return_type update(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
   controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+
+  controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
  protected:
   bool parserCommand(const std::string& name) override;
   bool parserObservation(const std::string& name) override;
 
   MotionCommandCfg cfg_;
+  DataLogger::SharedPtr dataLogger_;
 };
 
 }  // namespace legged
